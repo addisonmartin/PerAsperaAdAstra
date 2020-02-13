ac0637210c29e20a5e01712071cb7a38da043401
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2020-02-13 18:55:08 -0500

    Created a home controller with only an index action, and configured the homepage of the website to be home's index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Creates the routes for the home controller, whose only action is index.
+  get 'home/index'
+  # Sets the application's homepage to home's index action.
+  root 'home#index'
 end
