046c3ba8baae6bdc08ab9f3226c2b5829bbc4cbf
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2020-02-20 11:43:48 -0500

    Updated dates to be displayed as formatted strings with the day of week included.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
   # Enables pagination's frontend for all views.
   include Pagy::Frontend
+
+  # Formats a date to a string in the format: Monday, January 2, 2006
+  def format_date(date)
+    if date.nil?
+      date
+    else
+      date.strftime('%A, %B %-d, %Y')
+    end
+  end
+
 end
