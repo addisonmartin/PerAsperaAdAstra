23bb05af25a392e246378ad84ca0e16803998a32
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2020-02-20 10:58:47 -0500

    Updated satellite's controller to only have the show and index action, because the data is coming from an outside source and should not be modifiable by the user.

--- app/controllers/satellites_controller.rb
+++ app/controllers/satellites_controller.rb
@@ -1,5 +1,4 @@
 class SatellitesController < ApplicationController
-  before_action :set_satellite, only: [:show, :edit, :update, :destroy]
 
   # GET /satellites
   def index
@@ -11,49 +10,10 @@ class SatellitesController < ApplicationController
 
   # GET /satellites/1
   def show
-  end
-
-  # GET /satellites/new
-  def new
-    @satellite = Satellite.new
-  end
-
-  # GET /satellites/1/edit
-  def edit
-  end
-
-  # POST /satellites
-  def create
-    @satellite = Satellite.new(satellite_params)
-
-    if @satellite.save
-      redirect_to @satellite, notice: 'Satellite was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /satellites/1
-  def update
-    if @satellite.update(satellite_params)
-      redirect_to @satellite, notice: 'Satellite was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /satellites/1
-  def destroy
-    @satellite.destroy
-    redirect_to satellites_url, notice: 'Satellite was successfully destroyed.'
+    @satellite = Satellite.find(params[:id])
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_satellite
-    @satellite = Satellite.find(params[:id])
-  end
 
   # Only allow a trusted parameter "white list" through:
   def satellite_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 Rails.application.routes.draw do
 
   # Creates RESTful routes for satellites model.
-  resources :satellites
+  # Only includes the show and index action, because the data comes from an outside source and should not be user modifiable.
+  resources :satellites, only: [:show, :index]
 
   # Creates the routes for the home controller, whose only action is index.
   get 'home/index'
