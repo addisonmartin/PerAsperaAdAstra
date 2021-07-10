bfe2a56cefbc843a4510e818d95e491f15eff6af
Addison Martin <addisonmartin@hey.com>
2021-07-10 16:07:42 -0400

    Add three.js and sample scene to satellite's index view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,5 @@ import "stylesheets/application"
 
 const assets = require.context("../assets", true)
 const assetPath = name => assets(name, true)
+
+import "source/orbit_view"
---
+++ app/javascript/source/orbit_view.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+window.addEventListener("load", () => {
+    var orbit_view_element = document.getElementById(('orbit-view'))
+    if (orbit_view_element != null) {
+       const renderer = new THREE.WebGLRenderer();
+       var width = orbit_view_element.offsetWidth - 4; // Minus 4 to account for the 2px border on both left and right
+       var height = orbit_view_element.offsetHeight - 4; // Minus 4 to account for the 2px border on both top and left
+       renderer.setSize(width, height);
+       orbit_view_element.appendChild(renderer.domElement);
+
+        var scene = new THREE.Scene();
+
+        var camera = new THREE.PerspectiveCamera(75, width / height, 1, 10000);
+        camera.position.set( 0, 0, 100 );
+        camera.lookAt( 0, 0, 0 );
+
+        var controls = new OrbitControls(camera, orbit_view_element);
+        controls.target.set(0, 0, 0);
+
+        const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+        const points = [];
+        points.push( new THREE.Vector3(- 10, 0, 0 ));
+        points.push( new THREE.Vector3(0, 10, 0 ));
+        points.push( new THREE.Vector3(10, 0, 0 ));
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        const line = new THREE.Line(geometry, material);
+        scene.add(line);
+
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }
+
+        animate();
+    }
+});
