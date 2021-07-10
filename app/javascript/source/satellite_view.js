edae7f556e15931174946d24777de42de264f91f
Addison Martin <addisonmartin@hey.com>
2021-07-10 17:11:00 -0400

    Refactor satellite_view.js

--- app/javascript/source/satellite_view.js
+++ app/javascript/source/satellite_view.js
@@ -15,7 +15,7 @@ window.addEventListener("load", () => {
 
         var scene = new THREE.Scene();
 
-        var camera = new THREE.PerspectiveCamera(75, width / height, EARTH_RADIUS - 1, 100 * EARTH_RADIUS);
+        var camera = new THREE.PerspectiveCamera(75, width / height, EARTH_RADIUS / 100, 100 * EARTH_RADIUS);
         camera.position.set( 0, 0, EARTH_RADIUS * 2 );
         camera.lookAt( 0, 0, 0 );
 
@@ -37,28 +37,28 @@ window.addEventListener("load", () => {
             var gltfScene = gltf.scene;
 
             // Credit: https://stackoverflow.com/questions/52271397/centering-and-resizing-gltf-models-automatically-in-three-js
-            var bbox = new THREE.Box3().setFromObject(gltfScene);
-            var cent = bbox.getCenter(new THREE.Vector3());
-            var size = bbox.getSize(new THREE.Vector3());
+            var bounding_box = new THREE.Box3().setFromObject(gltfScene);
+            var center = bounding_box.getCenter(new THREE.Vector3());
+            var size = bounding_box.getSize(new THREE.Vector3());
 
-            //Rescale the object to normalized space
+            // Rescale the object to normalized space
             var maxAxis = Math.max(size.x, size.y, size.z);
             gltfScene.scale.multiplyScalar(EARTH_RADIUS / maxAxis);
-            bbox.setFromObject(gltfScene);
-            bbox.getCenter(cent);
-            bbox.getSize(size);
-            //Reposition to 0,0,0
-            gltfScene.position.copy(cent).multiplyScalar(-1);
+            bounding_box.setFromObject(gltfScene);
+            bounding_box.getCenter(center);
+            bounding_box.getSize(size);
+            // Reposition to 0, 0, 0
+            gltfScene.position.copy(center).multiplyScalar(-1);
 
             scene.add(gltfScene);
 
         }, function (xhr) {
-            console.log('Earth.glb ' + ( xhr.loaded / xhr.total * 100 ) + '% loaded');
+            console.log('Earth.glb ' + (xhr.loaded / xhr.total * 100 ) + '% loaded');
         }, undefined, function (error) {
             console.error(error);
         });
 
-        var light = new THREE.AmbientLight( 0xffffff );
+        var light = new THREE.AmbientLight(0xffffff);
         light.intensity = 5;
         scene.add(light);
 
